39b091aa4b43bebae37ab58dd458b05db88406cf
FadyEmad01 <fadyemad933@gmail.com>
2026-07-03 00:47:16 +0300

    5

--- src/components/NoiseOverlay.tsx
+++ src/components/NoiseOverlay.tsx
@@ -2,7 +2,7 @@
 
 export default function NoiseOverlay() {
   return (
-    <div className="pointer-events-none fixed inset-0 z-[9999999999999999] h-full w-full overflow-hidden opacity-[0.08]">
+     <div className="pointer-events-none fixed inset-0 z-[9999999999999999] h-full w-full overflow-hidden">
       <div
         className="absolute -left-[100%] -top-[100%] h-[300%] w-[300%] animate-noise bg-repeat"
         style={{
